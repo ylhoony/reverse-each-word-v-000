5ae443385b35a58993704357885a42132fbb8c18
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 19:18:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  arr = sentence.split(" ").collect do |e|
-    e.reverse
-  end
-  arr.join(" ")
+  # arr = sentence.split(" ").collect do |e|
+  #   e.reverse
+  # end
+  # arr.join(" ")
+  sentence.split.collect { |e| e.reverse}.join(" ")
 end
